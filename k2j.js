7de2b7c27486b6b1d47e496ddebc6f99f7e59967
Park June Chul <pjc0247@naver.com>
2014-11-29 00:11:46 +0900

    Update k2j.js

--- k2j.js
+++ k2j.js
@@ -1,9 +1,9 @@
 var keymap = [
-	'ㅁ','ㅠ','ㅊ','ㅇ','ㄷ','ㄹ','ㅎ','ㅗ','ㅑ','ㅓ','ㅏ','ㅣ','ㅡ','ㅜ','ㅐ','ㅔ','ㅂ','ㄱ','ㄴ','ㅅ','ㅕ','ㅍ','ㅈ','ㅌ','ㅛ','ㅋ'];
+  'ㅁ','ㅠ','ㅊ','ㅇ','ㄷ','ㄹ','ㅎ','ㅗ','ㅑ','ㅓ','ㅏ','ㅣ','ㅡ','ㅜ','ㅐ','ㅔ','ㅂ','ㄱ','ㄴ','ㅅ','ㅕ','ㅍ','ㅈ','ㅌ','ㅛ','ㅋ'];
 var initialized = false;
 
 var e2k = function(ch){
-	return keymap[ch - 65];
+  return keymap[ch - 65];
 }
 
 var cho_map = ["아","카까","가","사","자","타차짜따","다","나","하","바","파빠","마","라"];
@@ -42,82 +42,82 @@ var jong = function(a){
 }
 
 var break_map = function(map, func){
-	for(var i=0;i<map.length;i++){
-		for(var j=0;j<map[i].length;j++){
-			map[i] = map[i].replace(map[i][j], func(map[i][j]));
-		}
-	}
+  for(var i=0;i<map.length;i++){
+    for(var j=0;j<map[i].length;j++){
+      map[i] = map[i].replace(map[i][j], func(map[i][j]));
+    }
+  }
 }
 var index_of = function(map, ch){
-	for(var i=0;i<map.length;i++){
-		for(var j=0;j<map[i].length;j++){
-			if( map[i][j] == ch )
-				return i;
-		}
-	}
+  for(var i=0;i<map.length;i++){
+    for(var j=0;j<map[i].length;j++){
+      if( map[i][j] == ch )
+        return i;
+    }
+  }
 }
 var init = function(){
-	break_map( cho_map, cho );
-	break_map( joong_map, joong );
-	break_map( jong_map, jong );
-	
-	initialized = true;
+  break_map( cho_map, cho );
+  break_map( joong_map, joong );
+  break_map( jong_map, jong );
+  
+  initialized = true;
 }
 var special_case = function(ch1, ch2, ch3){
-	switch( ch2 ){
-		case '-':
-			if( ch3 != '-' && ch1 != '-' )
-				return "う";
-			else if( ch1 != '-' )
-				return "-";
-			else
-				return "";
-		case '.':
-			return "。";
-		case '와':
-			return 'わ';
-		case '워':
-			return 'を';
-	}
-	return null;
+  switch( ch2 ){
+    case '-':
+      if( ch3 != '-' && ch1 != '-' )
+        return "う";
+      else if( ch1 != '-' )
+        return "-";
+      else
+        return "";
+    case '.':
+      return "。";
+    case '와':
+      return 'わ';
+    case '워':
+      return 'を';
+  }
+  return null;
 }
 var k2j = function(str){
   var result = "";
   
   if(initialized == false) init();
 
-	for(var i=0;i<str.length;i++){
-		var ch = str[i];
+  for(var i=0;i<str.length;i++){
+    var ch = str[i];
 
-		/* 특수 케이스 */
-		var special = special_case(str[i-1], ch, str[i+1]);
-		if( special != null ){
-			result += special;
-			continue;
-		}
-		
-		/* 가나 변환 */
-		var col = index_of( cho_map, cho(ch) );
-		var row = index_of( joong_map, joong(ch) );
-		var gana = hiragana_map[ col * 13 + row ];
+    /* 특수 케이스 */
+    var special = special_case(str[i-1], ch, str[i+1]);
+    if( special != null ){
+      result += special;
+      continue;
+    }
+    
+    /* 가나 변환 */
+    var col = index_of( cho_map, cho(ch) );
+    var row = index_of( joong_map, joong(ch) );
+    var gana = hiragana_map[ col * 13 + row ];
 
-		if( gana != null )
-			result += gana;
-		else
-			result += ch;
+    if( gana != null )
+      result += gana;
+    else
+      result += ch;
 
-		/* 받침 붙이기 */
-		var ad = index_of( jong_map, jong(ch) );
-		switch( ad ){
-			case 0:
-				result += "ん";
-				break;
-			case 1:
-				result += "っ";
-				break;
-		}
-	}
+    /* 받침 붙이기 */
+    var ad = index_of( jong_map, jong(ch) );
+    switch( ad ){
+      case 0:
+        result += "ん";
+        break;
+      case 1:
+        result += "っ";
+        break;
+    }
+  }
 
-	return result;
+  return result;
 }
  
